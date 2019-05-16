337aacfb4436d2b78a69b0c730e009d47dbc7350
thomas zangari <tomsa78@hotmail.it>
2019-05-16 12:35:39 -0500

    update commit

--- angular/project/src/app/app.module.ts
+++ angular/project/src/app/app.module.ts
@@ -22,7 +22,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
