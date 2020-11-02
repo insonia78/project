7a35fa69dae4cbdd0d7b33a69470c643784a1f13
tomsa78 <tomsa78@hotmail.it>
2020-11-02 17:28:04 -0500

    finished project

--- dating_site/datingsite/datingsite/src/main/java/com/example/datingSite/repository/PhotosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.datingSite.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.datingSite.model.Photos;
-public interface PhotosRepository extends JpaRepository<Photos,Long> {
-    
-}
